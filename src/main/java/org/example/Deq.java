e77098f53bf8f375b229e2f77df1bc01423c133c
dreik84 <dreik84@mail.ru>
2025-07-04 16:45:59 +0300

    Add Deq.java

---
+++ src/main/java/org/example/Deq.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Deq {
+
+    private final int[] deququ;
+    private int size;
+    private int left;
+    private int right;
+
+    public Deq(int maxSize) {
+        deququ = new int[maxSize];
+        size = 0;
+        left = -1;
+        right = -1;
+    }
+
+    public void insertLeft(int num) {
+
+        if (isFull()) throw new RuntimeException();
+        left = (left == -1) ? deququ.length - 1 : left - 1;
+        deququ[left] = num;
+        size++;
+
+        System.out.println(Arrays.toString(deququ)); // diagnostic
+    }
+
+    public void insertRight(int num) {
+
+        if (isFull()) throw new RuntimeException();
+
+        right = (right == deququ.length - 1) ? 0 : right + 1;
+
+        deququ[right] = num;
+        size++;
+
+        System.out.println(Arrays.toString(deququ)); // diagnostic
+    }
+
+    public int removeLeft() {
+
+        if (isEmpty()) throw new RuntimeException();
+
+        int res = deququ[left];
+        left = (left == deququ.length - 1) ? 0 : left + 1;
+        size--;
+
+        System.out.println(Arrays.toString(deququ)); // diagnostic
+        return res;
+    }
+
+    public int removeRight() {
+
+        if (isEmpty()) throw new RuntimeException();
+
+        int res = deququ[right];
+        right = (right == 0) ? deququ.length - 1 : right - 1;
+        size--;
+
+        System.out.println(Arrays.toString(deququ)); // diagnostic
+        return res;
+    }
+
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public boolean isFull() {
+        return (size == deququ.length - 1);
+    }
+}
