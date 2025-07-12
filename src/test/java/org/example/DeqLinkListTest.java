91a06935c4bcd9e95b1bde8eb7a7a3ab02150f96
dreik84 <dreik84@mail.ru>
2025-07-12 10:20:10 +0300

    Add DeqLinkListTest.java

---
+++ src/test/java/org/example/DeqLinkListTest.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeqLinkListTest {
+
+    private DeqLinkList deq;
+
+    @BeforeEach
+    void init() {
+        deq = new DeqLinkList();
+    }
+
+    @Test
+    void insertLeft() {
+        deq.insertLeft(1);
+
+        System.out.println(deq);
+
+        int expected = 1;
+        int actual = deq.removeLeft();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void insertRight() {
+        deq.insertRight(1);
+
+        System.out.println(deq);
+
+        int expected = 1;
+        int actual = deq.removeRight();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isEmpty() {
+        assertTrue(deq.isEmpty());
+    }
+}
